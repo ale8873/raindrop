06acf646d33c0aef830ae32cd98911173e341f9f
Alessandro Vicentini <ale8873@gmail.com>
2018-01-20 15:54:32 +0100

    -

--- controllers/ApiController.php
+++ controllers/ApiController.php
@@ -12,7 +12,7 @@ class ApiController extends Controller
     public function actionInfoByEan($ean){
         header('Content-Type: application/json');
         if(!$keepa = Keepa::find()->where(['ean' => $ean])->one()){
-            $results = file_get_contents("https://api.keepa.com/product?key=70o31a1v29s60ojujqsom3cjms7h8nrr0kgo7pdgahv7hq4f2m5ml2jk8ha7la1t&domain=8&code=$ean");
+            $results = file_get_contents("https://api.keepa.com/product?key=<<&domain=8&code=$ean");
             echo $results;
             $keepa = new Keepa();
             $keepa->ean = $ean;
